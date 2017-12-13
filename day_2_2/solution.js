86878f1a2a43f7b66d01b889117348f2c2bd9a88
Stråberg <jacob.straberg@accenture.com>
2017-12-13 13:26:41 +0100

    commit

--- day_2_2/solution.js
+++ day_2_2/solution.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
-// var input = fs.readFileSync('input.txt','utf8');
-var input = '5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5'; //checksum = 18
+var input = fs.readFileSync('input.txt', 'utf8');
+// var input = '5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5' //checksum = 18
 if (typeof input === 'undefined') {
     throw new Error('Invalid input: no input provided.');
 }
@@ -14,15 +14,17 @@ function solve() {
     var columns = [];
     var num_of_nums;
     var numbers = [];
-    var sortedArray;
     var num_of_remainders;
     var remainders = [];
     var min;
     var max;
     var remainder;
     var sum = 0;
+    console.log('Rows: ' + rows);
+    console.log('Rows type of: ' + typeof (rows));
     for (var i = 0; i < num_of_rows; i++) {
         columns = rows[i].split('\t');
+        num_of_columns = columns.length;
         for (var j = 0; j < num_of_columns; j++) {
             var number = parseInt(columns[j]);
             numbers.push(number);
@@ -31,17 +33,22 @@ function solve() {
         console.log('Sorted numbers: ' + numbers);
         num_of_columns = columns.length;
         num_of_nums = numbers.length;
+        var m = 0;
         repeatLoop: for (var k = 0; k < num_of_nums; k++) {
-            if (numbers[0] % numbers[k + 1] === 0) {
-                numbers[0] < numbers[k + 1] ? (max = numbers[k + 1], min = numbers[0]) : (max = numbers[0], min = numbers[k + 1]);
+            if (numbers[m] % numbers[k + 1] === 0) {
+                numbers[m] < numbers[k + 1] ? (max = numbers[k + 1], min = numbers[m]) : (max = numbers[m], min = numbers[k + 1]);
                 remainder = max / min;
                 remainders.push(remainder);
                 console.log('Row: ' + i + '| min: ' + min + '| max: ' + max + '| remainder: ' + remainder);
                 numbers = [];
+                break repeatLoop;
+            }
+            else if (m === num_of_nums - 1) {
                 break;
             }
-            else {
-                numbers.splice(0, 1);
+            else if (k === num_of_nums - 1) {
+                k = m;
+                m++;
                 continue repeatLoop;
             }
         }
--- day_2_2/solution.ts
+++ day_2_2/solution.ts
@@ -1,8 +1,8 @@
 var fs = require('fs');
 
-// var input = fs.readFileSync('input.txt','utf8');
+var input = fs.readFileSync('input.txt','utf8');
 
- var input = '5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5' //checksum = 18
+// var input = '5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5' //checksum = 18
 
 if (typeof input === 'undefined') {
     throw new Error('Invalid input: no input provided.');
@@ -23,8 +23,6 @@ if (!input.length) {
     let num_of_nums: number
     let numbers: Array<number> = []
 
-    let sortedArray: Array<number>
-
     let num_of_remainders: number
     let remainders: Array<number> = []
 
@@ -35,10 +33,13 @@ if (!input.length) {
 
     let sum: number = 0
 
+    console.log('Rows: ' + rows);
+    console.log('Rows type of: ' + typeof(rows))
+
     for (let i=0; i<num_of_rows; i++) {
       
       columns = rows[i].split('\t')
-      
+      num_of_columns = columns.length
       for (let j=0; j<num_of_columns; j++) {
         
         let number = parseInt(columns[j])
@@ -52,12 +53,13 @@ if (!input.length) {
       num_of_columns = columns.length
       num_of_nums = numbers.length
 
+      let m = 0;
         repeatLoop:
         for (let k = 0; k<num_of_nums; k++) {
 
-          if (numbers[0]%numbers[k+1]===0) {
+          if (numbers[m]%numbers[k+1]===0) {
 
-            numbers[0]<numbers[k+1] ? ( max = numbers[k+1], min = numbers[0] ) : ( max = numbers[0], min = numbers[k+1])
+            numbers[m]<numbers[k+1] ? ( max = numbers[k+1], min = numbers[m] ) : ( max = numbers[m], min = numbers[k+1])
 
             remainder = max/min
 
@@ -67,15 +69,19 @@ if (!input.length) {
 
             numbers = []
 
-            break;
+            break repeatLoop
 
-          } else {
-            numbers.splice(0,1)
-            continue repeatLoop
+          }
+          else if (m === num_of_nums - 1) {
+            break
           }
 
+          else if (k === num_of_nums - 1) {
+            k=m
+            m++
+            continue repeatLoop
+          }          
         }
-
       }
 
     num_of_remainders = remainders.length
